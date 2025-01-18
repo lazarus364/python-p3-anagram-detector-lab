caa9d7e1858c909ed0e8a2fa2b79522b8c0cdb07
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2025-01-18 15:52:41 +0300

    python

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+# lib/anagram.py  
+
+class Anagram:  
+    def __init__(self, word):  
+        self.word = word  
+
+    def match(self, possible_anagrams):  
+        matches = []  
+        sorted_word = sorted(self.word)  
+
+        for candidate in possible_anagrams:  
+            if sorted(candidate) == sorted_word:  
+                matches.append(candidate)  
+
+        return matches
+    
+listen = Anagram("listen")  
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))  
+    
